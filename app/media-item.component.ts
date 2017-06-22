7fc02e25b98b9e932d1d1d4331a7f068397544a4
Your Name <you@example.com>
2017-06-22 22:06:01 +0200

    learn how to use component in component, bind a property to template

---
+++ app/media-item.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'mk-media-item',
+    templateUrl: 'app/media-item.component.html',
+    styleUrls: ['app/media-item.component.css']
+})
+export class MediaItemComponent {
+    name = "The Redemption";
+
+    wasWatched = function () {
+        return true;
+    }
+}
