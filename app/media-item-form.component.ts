adab1fc1bfa8d104d41889087cae02e74c95a69e
Your Name <you@example.com>
2017-07-29 19:41:10 +0200

    learn how build and provide a service, and how to use a service in two different components

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Validators, FormBuilder, Form} from '@angular/forms'
+import {MediaItemService} from "./media-item.service";
 
 @Component({
   selector: 'mk-media-item-form',
@@ -9,7 +10,9 @@ import {Validators, FormBuilder, Form} from '@angular/forms'
 export class MediaItemFormComponent {
     form;
 
-    constructor(private formBuilder: FormBuilder){}
+    constructor(
+        private formBuilder: FormBuilder,
+        private mediaItemService: MediaItemService){}
 
     ngOnInit(){
         this.form = this.formBuilder.group({
@@ -43,6 +46,6 @@ export class MediaItemFormComponent {
     }
 
     onSubmit(mediaItem){
-        console.log(mediaItem);
+        this.mediaItemService.add(mediaItem);
     }
 }
